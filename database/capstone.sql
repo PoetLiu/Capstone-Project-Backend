1e1b8b5ef86911974b4f99a5f68359644b7dbbc8
AlbertLiu <457566268@qq.com>
2024-09-19 16:19:02 -0400

    photoUrl is optional in users table.

--- database/capstone.sql
+++ database/capstone.sql
@@ -187,7 +187,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `photoUrl` varchar(100) NOT NULL,
+  `photoUrl` varchar(100) NULL,
   `password_hash` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
