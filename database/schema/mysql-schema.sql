9b2f3f87b40367bee52e6db2377e58a9a7d5aec8
AlbertLiu <457566268@qq.com>
2024-11-07 20:23:53 -0500

    remove utf8mb4_0900_ai_ci collation for db.

--- database/schema/mysql-schema.sql
+++ database/schema/mysql-schema.sql
@@ -110,7 +110,7 @@ CREATE TABLE `coupons` (
   `min_amount` int NOT NULL,
   `discount` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `failed_jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
